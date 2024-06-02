75cafd28a04b6b59470c030ae31fc27149ce394b
minseobshin <mshin5211@gmail.com>
2024-06-01 22:48:08 -0500

    nothing

--- src/_lib/data.ts
+++ src/_lib/data.ts
@@ -60,21 +60,18 @@ export const branchData = [
     {
         id: 1,
         title: 'Professional',
-        href: '/branch-1',
         src: '',
         description: 'The professional branch focuses on advancing technology by organizing events, workshops, and networking opportunities for engineering professionals and students.'
     },
     {
         id: 2,
         title: 'Technical',
-        href: '/branch-2',
         src: '',
         description: 'The technical branch focuses on enhancing technical skills, organizing workshops and events, and providing practical, hands-on experiences in various engineering and technological fields.'
     },
     {
         id: 3,
         title: 'Membership / Social',
-        href: '/branch-3',
         src: '',
         description: 'The membership/social branch focuses on building a vibrant community by organizing social events, networking opportunities, and fostering connections among its members.'
     }
