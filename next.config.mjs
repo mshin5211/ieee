b372ffc5083db69edabe9da1b63e42992a65ebc8
minseobshin <mshin5211@gmail.com>
2024-08-25 23:45:55 -0500

    redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/join',
+                destination: '/',
+                permanent: true,
+            }
+        ]
+    }
+};
 
 export default nextConfig;
